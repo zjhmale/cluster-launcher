6a7ee6ab1901945dba8b9ae4103cf1845b657751
zjhsdtc <zjhsdtc@gmail.com>
2020-03-09 22:39:35 +0800

    chore: example with etcd client

--- examples/etcd/container.go
+++ examples/etcd/container.go
@@ -6,7 +6,9 @@ import (
 	"os"
 	"os/signal"
 	"sync"
+	"time"
 
+	client "github.com/coreos/etcd/clientv3"
 	. "github.com/zjhmale/cluster-launcher/etcd"
 )
 
@@ -19,28 +21,58 @@ func main() {
 	ctx := context.Background()
 	network, err := NewEtcdNetwork(ctx, clusterName)
 	if err != nil {
-		fmt.Printf("Create network error %v", err)
+		fmt.Printf("Create network error %v\n", err)
 	}
 
 	wg := &sync.WaitGroup{}
 	listener := NewEtcdListener(wg)
 	c, err := NewEtcdContainer(ctx, wg, clusterName, listener, "etcd", []string{"etcd"})
 	if err != nil {
-		fmt.Printf("Create container error %v", err)
+		fmt.Printf("Create container error %v\n", err)
 	}
 	if err := c.Start(); err != nil {
-		fmt.Printf("Start container error %v", err)
+		fmt.Printf("Start container error %v\n", err)
 	}
 	wg.Wait()
+
+	endpoint, err := c.ClientEndpoint()
+	if err != nil {
+		fmt.Printf("Get client endpoint error %v\n", err)
+	}
+
+	cli, err := client.New(client.Config{
+		Endpoints: []string{endpoint.Host},
+		DialTimeout: 5 * time.Second,
+	})
+
+	if err !=nil {
+		fmt.Printf("Create etcd client error %v\n", err)
+	}
+
+	kv := client.NewKV(cli)
+	_, err = kv.Put(context.TODO(), "key", "xyz")
+	if err != nil {
+		fmt.Printf("Put kv error %v\n", err)
+	}
+
+	resp, err := kv.Get(context.TODO(), "key")
+	if err != nil {
+		fmt.Printf("Get kv error %v\n", err)
+	}
+	pair := resp.Kvs[0]
+	fmt.Printf("Key %s Value %s\n", pair.Key, pair.Value)
 
 	go func() {
 		<-stop
 		if err := c.Close(); err != nil {
-			fmt.Printf("Stop container error %v", err)
+			fmt.Printf("Stop container error %v\n", err)
+			os.Exit(1)
 		}
 		if err := network.Remove(); err != nil {
-			fmt.Printf("Remove network error %v", err)
+			fmt.Printf("Remove network error %v\n", err)
+			os.Exit(1)
 		}
+		cli.Close()
 		os.Exit(0)
 	}()
 
--- go.mod
+++ go.mod
@@ -3,9 +3,24 @@ module github.com/zjhmale/cluster-launcher
 go 1.13
 
 require (
+	github.com/coreos/etcd v3.3.18+incompatible
+	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf // indirect
+	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
 	github.com/docker/docker v0.7.3-0.20190506211059-b20a14b54661
 	github.com/docker/go-connections v0.4.0
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/golang/protobuf v1.3.4 // indirect
+	github.com/google/uuid v1.1.1 // indirect
 	github.com/stretchr/testify v1.4.0
 	github.com/testcontainers/testcontainers-go v0.3.1
 	github.com/tevino/abool v0.0.0-20170917061928-9b9efcf221b5
+	go.uber.org/multierr v1.5.0 // indirect
+	go.uber.org/zap v1.14.0 // indirect
+	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
+	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/tools v0.0.0-20200308013534-11ec41452d41 // indirect
+	google.golang.org/grpc v1.26.0 // indirect
+	honnef.co/go/tools v0.0.1-2020.1.3 // indirect
 )
